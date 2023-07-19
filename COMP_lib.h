0b3a6f9306a171e783be5e674369917043af7dcd
Daniel Krueger <daniel.krueger@stud.thga.de>
2023-07-19 18:49:22 +0200

    Funktionen zum berechnen der 2-norm deklariert und getestet & Funktion zur addition deklariert

--- COMP_lib.h
+++ COMP_lib.h
@@ -1,13 +1,28 @@
 /* COMP_lib.h */
 
-struct COMP_int 
+#ifndef COMP_LIB_H
+#define COMP_LIB_H
+
+typedef struct  
 {
 	int real_ganz ;
+	char rechenzeichen_ganz ;
 	int imaginaer_ganz ;
-} ;
+} COMP_int ;
 
-struct COMP_float
+typedef struct
 { 
 	float real_gleitkomma ;
+	char rechenzeichen_gleitkomma ;
 	float imaginaer_gleitkomma ;
-} ;
+} COMP_float ;
+
+int COMP_berechnung_2_norm_int ( COMP_int komplex_ganzzahl ) ;
+
+float COMP_berechnung_2_norm_float ( COMP_float komplex_gleitkomma ) ;
+
+void COMP_addition_komplex ( void *komplexe_zahl_1 , void *komplexe_zahl_2 , void *komplex_ergebniss ) ;
+
+
+#endif
+
