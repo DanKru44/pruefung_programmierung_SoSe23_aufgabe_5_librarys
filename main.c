f53eabd5d4f71ccca179e74b5d29fe2a56290c22
Daniel Krueger <daniel.krueger@stud.thga.de>
2023-07-23 12:04:31 +0200

    Testausgab der eulerform und der fehlermelung der 2-norm eingefügt

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main ()
 	COMP_komplex zahl_int_1 =  
 	{
 		.typ = COMP_int ,
-		.int_daten.real = 2 ,
+		.int_daten.real = -2 ,
 		.int_daten.imaginaer = 3 
 	} ;
 		
@@ -30,21 +30,24 @@ int main ()
 	COMP_komplex zahl_float_2 = 
 	{
 		.typ = COMP_float ,
-		.float_daten.real = 3.21 ,
+		.float_daten.real = -3.21 ,
 		.float_daten.imaginaer = 9.232345 
 	} ;
 
 	COMP_komplex zahl_ergebniss ;
 
 	float ergebniss_2_norm_float ;
-
 	
-	ergebniss_2_norm_int = COMP_berechnung_2_norm_int ( zahl_int_2 ) ;
+	ergebniss_2_norm_int = COMP_berechnung_2_norm_int ( zahl_int_1 ) ;
 	printf ( "Die 2-Norm der integer Zahl beträgt: %i\n" , ergebniss_2_norm_int ) ;
+
+	ergebniss_2_norm_int = COMP_berechnung_2_norm_int ( zahl_float_1 ) ;
 
 	ergebniss_2_norm_float = COMP_berechnung_2_norm_float ( zahl_float_2 ) ;
 	printf ( "Die 2-Norm der float Zahl beträgt: %f\n" , ergebniss_2_norm_float ) ;
 	
+	ergebniss_2_norm_float = COMP_berechnung_2_norm_float ( zahl_int_2 ) ;
+
 	COMP_addition_komplex ( zahl_int_1 , zahl_int_2 , &zahl_ergebniss ) ;
 	printf ( "Die addition der beiden int Zahlen ergibt : %i+%ii\n" , zahl_ergebniss.int_daten.real , zahl_ergebniss.int_daten.imaginaer ) ;
 	
@@ -55,6 +58,12 @@ int main ()
 	printf ( "Die addition der beiden float Zahlen ergibt : %f+%fi\n" , zahl_ergebniss.float_daten.real , zahl_ergebniss.float_daten.imaginaer ) ;
 
 	COMP_ausgabe_eulerform ( zahl_int_1 ) ;
+
+	COMP_ausgabe_eulerform ( zahl_int_2 ) ;
+
+	COMP_ausgabe_eulerform ( zahl_float_1 ) ;
+
+	COMP_ausgabe_eulerform ( zahl_float_2 ) ;
 
 	return 0 ;
 }
