89d74bda2a186c3e22628ea85cb4c65a1df1e1ed
Daniel Krueger <daniel.krueger@stud.thga.de>
2023-07-23 12:03:49 +0200

    Fehlermeldung funktion addition und fehlermeldung entfernt da dises nicht erreicht werden im test, kompilation bricht vorher schon ab bei falscher verwendung

--- COMP_lib.c
+++ COMP_lib.c
@@ -47,7 +47,7 @@ void COMP_addition_komplex ( COMP_komplex komplexe_zahl_1 , COMP_komplex komplex
 		}
 	}
 
-	else if	( ( komplexe_zahl_1.typ ==  COMP_float && komplexe_zahl_2.typ == COMP_int ) || ( komplexe_zahl_1.typ == COMP_int && komplexe_zahl_2.typ == COMP_float ) )
+	else
 	{
 		komplex_ergebniss -> typ == COMP_float ;
 
@@ -64,11 +64,6 @@ void COMP_addition_komplex ( COMP_komplex komplexe_zahl_1 , COMP_komplex komplex
 		}
 	}
 	
-	else
-	{
-		printf ( "Ungültige Datenstruktur einer oder beider komplexen Zahlen\n" ) ;
-	}
-	
 }
 
 // r*e^i*phi = r * ( cos(phi) + i * sin(phi) )
@@ -76,71 +71,34 @@ void COMP_ausgabe_eulerform ( COMP_komplex komplexe_euler )
 {
 	if ( komplexe_euler.typ == COMP_int ) 
 	{
-		float betrag_int = COMP_berechnung_2_norm_int ( komplexe_euler ) ;
-		float winkel_int = winkel_int = atan2 ( komplexe_euler.int_daten.imaginaer , komplexe_euler.int_daten.real ) ;
+		int betrag_int = COMP_berechnung_2_norm_int ( komplexe_euler ) ;
+		int winkel_int = atan2 ( komplexe_euler.int_daten.imaginaer , komplexe_euler.int_daten.real ) ;
 		
 		if ( komplexe_euler.int_daten.real > 0 )	
 		{	
-			if ( winkel_int >= 0 )
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*%f = cos(%f) + i * sin(%f)\n" , betrag_int , winkel_int , winkel_int , winkel_int ) ;
-			}
-
-			else 
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*(%f) = cos(%f) + i * sin(%f)\n" , betrag_int , winkel_int , winkel_int , winkel_int ) ;
-			}
+				printf ( "Die Euler-Form lautet: %i * e^i*(%i) = cos(%i) + i * sin(%i)\n" , betrag_int , winkel_int , winkel_int , winkel_int ) ;
 		}
 
 		else
 		{
-			if ( winkel_int >= 0 )
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*%f+%lc = cos(%f+%lc) + i * sin(%f+%lc)\n" , betrag_int , winkel_int , L'\u03C0' , winkel_int , L'\u03C0' , winkel_int , L'\u03C0' ) ;
-			}
-
-			else 
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*(%f+%lc) = cos(%f+%lc) + i * sin(%f+%lc)\n" , betrag_int , winkel_int , L'\u03C0' , winkel_int , L'\u03C0' , winkel_int , L'\u03C0' ) ;
-			}
+				printf ( "Die Euler-Form lautet: %i * e^i*(%i+%s) = cos(%i+%s) + i * sin(%i+%s)\n" , betrag_int , winkel_int , "\u03C0" , winkel_int , "\u03C0" , winkel_int , "\u03C0" ) ;
 		}
 
 	}
 
-	else if ( komplexe_euler.typ == COMP_float )
+	else 
 	{
 		float betrag_float = COMP_berechnung_2_norm_float ( komplexe_euler ) ;
 		float winkel_float = atan2 ( komplexe_euler.float_daten.imaginaer , komplexe_euler.float_daten.real ) ;
 
 		if ( komplexe_euler.float_daten.real > 0 )	
 		{	
-			if ( winkel_float >= 0 )
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*%f = cos(%f) + i * sin(%f)\n" , betrag_float , winkel_float , winkel_float , winkel_float ) ;
-			}
-
-			else 
-			{
 				printf ( "Die Euler-Form lautet: %f * e^i*(%f) = cos(%f) + i * sin(%f)\n" , betrag_float , winkel_float , winkel_float , winkel_float ) ;
-			}
 		}
 
 		else
 		{
-			if ( winkel_float >= 0 )
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*%f+%lc = cos(%f+%lc) + i * sin(%f+%lc)\n" , betrag_float , winkel_float , L'\u03C0' , winkel_float , L'\u03C0' , winkel_float , L'\u03C0' ) ;
-			}
-
-			else 
-			{
-				printf ( "Die Euler-Form lautet: %f * e^i*(%f+%lc) = cos(%f+%lc) + i * sin(%f+%lc)\n" , betrag_float , winkel_float , L'\u03C0' , winkel_float , L'\u03C0' , winkel_float , L'\u03C0' ) ;
-			}
+				printf ( "Die Euler-Form lautet: %f * e^i*(%f+%s) = cos(%f+%s) + i * sin(%f+%s)\n" , betrag_float , winkel_float , "\u03C0" , winkel_float , "\u03C0" , winkel_float , "\u03C0" ) ;
 		}
-	}
-
-	else
-	{
-		printf ( " Ungültige Datenstruktur der komplexen Zahl!\n" ) ;
 	}
 }
